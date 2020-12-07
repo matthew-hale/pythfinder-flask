2adca564629c4709275cf66a86e2a9bbe229cb49
Matt Hale <matthew.hale@protonmail.com>
2020-12-07 18:55:58 -0500

    Add prototype for /character/equipment

--- src/pf-flask.py
+++ src/pf-flask.py
@@ -24,16 +24,23 @@ def character_name():
 
 @app.route("/character/equipment")
 def character_equipment():
-    filter_data = dict(request.args) if request.args else {}
-    return json.dumps(c.get_item(data = filter_data))
-
-@app.route("/character/equipment")
-def character_equipment_name(name):
-    out = None
-    for item in c.equipment:
-        if item["name"] == name:
-            out = json.dumps(item)
-    if not out:
-        abort(404)
-    else:
-        return out
+    #filter_data = dict(request.args) if request.args else {}
+    #return json.dumps(c.get_item(data = filter_data))
+    name = request.args.get("name") if request.args.get("name") else []
+    weight = request.args.get("weight") if request.args.get("weight") else {}
+    if weight:
+        weight = json.loads(str(request.args.get("weight")).replace("'", '"')) 
+    count = request.args.get("count") if request.args.get("count") else {}
+    if count:
+        count = json.loads(str(request.args.get("count")).replace("'", '"')) 
+    camp = request.args.get("camp") if request.args.get("camp") else []
+    on_person = request.args.get("on_person") if request.args.get("on_person") else []
+    location = request.args.get("location") if request.args.get("location") else []
+    notes = request.args.get("notes") if request.args.get("notes") else []
+    return json.dumps(c.get_item(name = name,
+                                 weight = weight,
+                                 count = count,
+                                 camp = camp,
+                                 on_person = on_person,
+                                 location = location,
+                                 notes = notes))
